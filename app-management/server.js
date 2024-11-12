8d35ab8e228653375da130e3fddde6c116690437
Katarzyna Rdzanek <kkrdzanek@gmail.com>
2024-11-12 10:16:05 +0100

    updated app label

--- app-management/server.js
+++ app-management/server.js
@@ -342,7 +342,7 @@ app.use((err, req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Automate for QA API');
+  res.send('Welcome to the Automate 4 QA API');
 });
 
 app.get('*', (req, res) => {
