6561c5ee83909a7b4f075f23bca9b3747239707b
Katarzyna Rdzanek <kkrdzanek@gmail.com>
2024-11-17 16:12:40 +0100

    chrome check56 corrected file naming

--- webdriverio_test_project/test/specs/positiveLogintest.js
+++ webdriverio_test_project/test/specs/positiveLogintest.js
@@ -1,7 +1,7 @@
 import LoginModal from '../pages/loginModal.js';
 import NavTabs from '../pages/navTabs.js';
 import { loginInputs } from '../resources/loginInputs.js';
-import Home from '../pages/Home.js'
+import Home from '../pages/home.js';
 
 describe('Positive Login and Navigation Functionality', () => {
 
